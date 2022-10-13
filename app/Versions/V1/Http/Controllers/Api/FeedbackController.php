74e395c51a9cdf1f1b512c5f55aab7cd6ef6635b
midnightelf <abdulovk20@gmail.com>
2022-10-13 08:38:23 +0300

    Add feedback getters

--- app/Versions/V1/Http/Controllers/Api/FeedbackController.php
+++ app/Versions/V1/Http/Controllers/Api/FeedbackController.php
@@ -3,9 +3,19 @@
 namespace App\Versions\V1\Http\Controllers\Api;
 
 use App\Versions\V1\Http\Controllers\Controller;
+use App\Versions\V1\Http\Resources\Collections\FeedbackCollection;
+use App\Versions\V1\Repositories\FeedbackRepository;
 use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
-    //
+    public function __construct(
+        public FeedbackRepository $repository
+    ) {
+    }
+
+    public function index(Request $request): FeedbackCollection
+    {
+        return new FeedbackCollection($this->repository->paginate(4));
+    }
 }
