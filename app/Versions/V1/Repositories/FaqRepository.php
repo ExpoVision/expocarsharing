173a976be972287ead61ff12a00f322c09ee9d4d
midnightelf <abdulovk20@gmail.com>
2022-10-13 19:52:14 +0300

    Fix MODEL const in repositories

--- app/Versions/V1/Repositories/FaqRepository.php
+++ app/Versions/V1/Repositories/FaqRepository.php
@@ -2,11 +2,14 @@
 
 namespace App\Versions\V1\Repositories;
 
+use App\Models\Faq;
 use App\Versions\V1\Contracts\RepositoryContract;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class FaqRepository extends RepositoryContract
 {
+    public const MODEL = Faq::class;
+
     public function paginate(?int $perPage): LengthAwarePaginator
     {
         return $this->builder->paginate($perPage);
