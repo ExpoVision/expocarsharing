35f4923bbe45c0797195f57541e5a8fac88b7a67
midnightelf18 <abdulov20@gmail.com>
2022-11-06 06:18:59 +0000

    Add unique constraint for orders

--- database/migrations/2022_10_12_035713_create_orders_table.php
+++ database/migrations/2022_10_12_035713_create_orders_table.php
@@ -17,8 +17,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('offer_id');
-            $table->foreignId('user_id')->constrained()
+            $table->foreignId('offer_id')->unique();
+            $table->foreignId('user_id')->unique()
+                ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
             $table->timestamp('started_at')->default(Carbon::now());
