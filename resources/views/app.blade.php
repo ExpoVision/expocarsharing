7eda9fe4e834ee212e417e3d93be4d20c840dde2
abdulaev_ii <abdulaev_ii@magnit.ru>
2023-04-10 21:20:27 +0000

    Add build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }}</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>carsharing</title>
+    <script defer="defer" src="/js/chunk-vendors.fba6630b.js"></script>
+    <script defer="defer" src="/js/app.0b21a225.js"></script>
+    <link href="/css/app.193b0c93.css" rel="stylesheet">
 </head>
 
-<body>
+<body><noscript><strong>We're sorry but carsharing doesn't work properly without JavaScript enabled. Please enable it to
+            continue.</strong></noscript>
     <div id="app"></div>
-
-    @vite('resources/js/app.js')
 </body>
 
 </html>
