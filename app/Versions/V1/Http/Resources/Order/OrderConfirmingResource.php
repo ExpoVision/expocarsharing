b250b640b8e3b831f7f03f11883b9e2c8bf06710
abdulaev_ii <abdulaev_ii@magnit.ru>
2023-05-30 16:51:49 +0000

    Fix confirming resource

--- app/Versions/V1/Http/Resources/Order/OrderConfirmingResource.php
+++ app/Versions/V1/Http/Resources/Order/OrderConfirmingResource.php
@@ -4,14 +4,5 @@ namespace App\Versions\V1\Http\Resources\Order;
 
 class OrderConfirmingResource extends OrderResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return new OrderResource($this);
-    }
+    public $collects = OrderResource::class;
 }
